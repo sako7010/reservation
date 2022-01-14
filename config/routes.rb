c41297afdba3546eb4d9b793298a54fd947ac24f
sako <naotolj.fox@gmail.com>
2022-01-14 17:32:29 +0900

    プロフィール画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :users
+  resources :users do
+    collection do
+      get "profile"
+    end
+  end
   resources :rooms
   resources :bookings do
     collection do 
